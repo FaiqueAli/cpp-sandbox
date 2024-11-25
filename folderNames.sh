7646d76b8f8defc12faca8307702d29a90005404
Faiq Ali Sayed <faiq.ali.sayed@sioux.eu>
2024-11-25 16:02:07 +0100

    difference between previous commit of the feature branch

--- folderNames.sh
+++ folderNames.sh
@@ -16,7 +16,7 @@ FOLDERS=("arithmetic_ops" "input_handler" )
 # Get the list of changed folders
 #CHANGED_FOLDERS=$(git diff --name-only $MAIN_BRANCH $FEATURE_BRANCH -- "${FOLDERS[@]}" | awk -F'/' '{print $1}' | sort -u)
 CHANGED_FOLDERS=$(git diff --name-only $FEATURE_BRANCH HEAD~1 -- "${FOLDERS[@]}" | awk -F'/' '{print $1}' | sort -u)
-
+echo $CHANGED_FOLDERS
 # Loop through each changed folder and run 'make' if there are changes
 for folder in $CHANGED_FOLDERS; do
     echo "Building folder: $folder"
