ece2104918bbafffb184144829258bd5a3d9915f
Faiq Ali Sayed <faiq.ali.sayed@sioux.eu>
2024-11-25 13:40:47 +0100

    run app using main branch

--- compile.sh
+++ compile.sh
@@ -1,6 +1,12 @@
-cd arithmetic_ops
+# cd arithmetic_ops
+# make
+# cd ../input_handler
+# make
+# cd ../main_logic
+
+echo 'inside script'
+cd main_logic
+
 make
-cd ../input_handler
-make
-cd ../main_logic
-make
+
+./main_logic
