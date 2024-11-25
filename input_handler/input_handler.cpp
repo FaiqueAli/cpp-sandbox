944de70581ff5d0cc460b2f09acc283ced635f02
Faiq Ali Sayed <faiq.ali.sayed@sioux.eu>
2024-11-25 14:06:41 +0100

    run app using main branch

--- input_handler/input_handler.cpp
+++ input_handler/input_handler.cpp
@@ -1,5 +1,5 @@
 #include "input_handler.h"
 
-int getInputA() { return 4; }
-int getInputB() { return 3; }
+int getInputA() { return 1; }
+int getInputB() { return 1; }
 char getOperator() { return '+'; }
