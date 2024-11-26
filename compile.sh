adf65894d69b3982123835ff1d034bed6a021416
Faiq Ali Sayed <faiq.ali.sayed@sioux.eu>
2024-11-26 18:07:13 +0100

    compile from master result should be 2

--- compile.sh
+++ compile.sh
@@ -1,14 +1,13 @@
-# cd arithmetic_ops
-# make
-# cd ../input_handler
-# make
-# cd ../main_logic
+cd arithmetic_ops
+make
+cd ../input_handler
+make
+cd ../main_logic
 
-# echo 'inside script'
-# # cd main_logic
+echo 'inside script'
+# cd main_logic
 
-# make
+make
 
-# ./main_logic
+./main_logic
 
-echo "This is master branch, msg from compile script"
