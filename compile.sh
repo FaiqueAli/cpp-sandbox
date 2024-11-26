cdca84bfd987bc51be20d5561b18cf5cdd0c39c3
Faiq Ali Sayed <faiq.ali.sayed@sioux.eu>
2024-11-26 17:49:19 +0100

    test message from script

--- compile.sh
+++ compile.sh
@@ -1,10 +1,11 @@
-cd arithmetic_ops
-make
+# cd arithmetic_ops
+# make
 
-cd ../input_handler
-make
+# cd ../input_handler
+# make
 
-cd ../main_logic
-make
-chmod 777 main_logic
-./main_logic
+# cd ../main_logic
+# make
+# chmod 777 main_logic
+# ./main_logic
+echo "this is test message from compile script"
