b1cf8fbe5b0dfcfffe67d32d38de0e1bd2d3879a
Faiq Ali Sayed <faiq.ali.sayed@sioux.eu>
2024-11-26 15:24:14 +0100

    change in input handler values

--- compile.sh
+++ compile.sh
@@ -1,9 +1,13 @@
 cd arithmetic_ops
+ls
 make
+
 cd ../input_handler
+ls
 make
 
 cd ../main_logic
+ls
 make
-
+ls
 ./main_logic
--- input_handler/input_handler.cpp
+++ input_handler/input_handler.cpp
@@ -1,6 +1,6 @@
 #include "input_handler.h"
 
-int getInputA() { return 10; }
-int getInputB() { return 10; }
+int getInputA() { return 5; }
+int getInputB() { return 5; }
 char getOperator() { return '+'; }
 
