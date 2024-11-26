fe283b56529a01cc36fdd502cd826c561bd7777a
Faiq Ali Sayed <faiqueali.109@gmail.com>
2024-11-26 19:13:40 +0100

    compile using folderNames script ans should be 20

--- input_handler/input_handler.cpp
+++ input_handler/input_handler.cpp
@@ -1,6 +1,6 @@
 #include "input_handler.h"
 
-int getInputA() { return 1; }
-int getInputB() { return 1; }
+int getInputA() { return 10; }
+int getInputB() { return 10; }
 char getOperator() { return '+'; }
 
