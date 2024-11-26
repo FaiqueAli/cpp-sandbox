3fc10377cc7e573e033bdea1ec5890ea804974b7
Faiq Ali Sayed <faiq.ali.sayed@sioux.eu>
2024-11-26 18:45:37 +0100

    compile using folderNames script ans should be 5

--- folderNames.sh
+++ folderNames.sh
@@ -11,10 +11,13 @@ FOLDERS=("arithmetic_ops" "input_handler" "main_logic")
 CHANGED_FOLDERS=$(git diff --name-only $MAIN_BRANCH $FEATURE_BRANCH -- "${FOLDERS[@]}" | awk -F'/' '{print $1}' | sort -u)
 
 # Loop through each changed folder and run 'make' if there are changes
+pwd
 for folder in $CHANGED_FOLDERS; do
     echo "Building folder: $folder"
     cd $folder && make && cd ..
 done
+pwd
+
 # cd main_logic 
 # chmod 777 main_logic
 # echo 'the result is ' 
