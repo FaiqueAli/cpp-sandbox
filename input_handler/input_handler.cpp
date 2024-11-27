00bda5910e43d494e1fe35dbf293a2454d8d680a
Faiq Ali Sayed <faiq.ali.sayed@sioux.eu>
2024-11-27 14:40:00 +0100

    clean workspace after build

--- input_handler/input_handler.cpp
+++ input_handler/input_handler.cpp
@@ -1,6 +1,6 @@
 #include "input_handler.h"
 
-int getInputA() { return 3; }
-int getInputB() { return 3; }
+int getInputA() { return 4; }
+int getInputB() { return 4; }
 char getOperator() { return '+'; }
 
