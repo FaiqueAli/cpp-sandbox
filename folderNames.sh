9155f1f7fec6ef10db8dfac49c3929b452b15f46
Faiq Ali Sayed <faiq.ali.sayed@sioux.eu>
2024-11-28 11:35:50 +0100

    always run make file from main logic folder

--- folderNames.sh
+++ folderNames.sh
@@ -5,7 +5,7 @@ MAIN_BRANCH="origin/main"
 FEATURE_BRANCH="origin/$(git rev-parse --abbrev-ref HEAD)"
 
 # Folders to check for changes
-FOLDERS=("arithmetic_ops" "input_handler" "main_logic")
+FOLDERS=("arithmetic_ops" "input_handler")
 
 # Get the list of changed folders
 CHANGED_FOLDERS=$(git diff --name-only $MAIN_BRANCH HEAD~1 -- "${FOLDERS[@]}" | awk -F'/' '{print $1}' | sort -u)
@@ -17,9 +17,8 @@ for folder in $CHANGED_FOLDERS; do
     echo "Building folder: $folder"
     cd $folder && make && cd ..
 done
-pwd
-
 cd main_logic 
+make
 chmod 777 main_logic
 echo 'the result is ' 
 ./main_logic
