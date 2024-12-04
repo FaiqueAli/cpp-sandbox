9cdb2fd754484c37971345b192a79173410ad1b0
Faiq Ali Sayed <faiqueali.109@gmail.com>
2024-12-04 17:52:46 +0100

    update f branch

--- input_handler/input_handler.cpp
+++ input_handler/input_handler.cpp
@@ -1,5 +1,5 @@
 #include "input_handler.h"
 
-int getInputA() { return 1; }
-int getInputB() { return 1; }
+int getInputA() { return 5; }
+int getInputB() { return 5; }
 char getOperator() { return '+'; }
